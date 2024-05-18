f288364502d411b36ac2f695a76f1bf0deb880d0
Gage Hildebrandt <vadium3854@gmail.com>
2024-05-18 01:00:22 -0400

    added form and event listeners

---
+++ scripts.js
@@ -0,0 +1,24 @@
+let myLibrary = [''];
+
+function Book(title, author, pageCount, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.readStatus = readStatus;
+
+    this.info = function() {
+        let readStatusText = this.readStatus ? 'read' : 'not read yet';
+        return `${this.title} by ${this.author}, ${this.pageCount} pages, ${readStatusText}`;
+    };
+}
+
+function addBookToLibrary(title, author, pages, read) {
+
+}
+
+let newBookbtn = document.querySelector('#new-book-btn');
+newBookbtn.addEventListener('click', function() {
+    alert('HELLO WORLD');
+    // let newBookForm = document.querySelector('#new-book-form');
+    // newBookForm.style.display = block;
+})
