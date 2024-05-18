3b37a681e97ba09ba9437a610b84715e0e1ce78c
Gage Hildebrandt <vadium3854@gmail.com>
2024-05-18 15:25:13 -0400

    encountering bug on line 16, will come back to fix later

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 let myLibrary = [''];
 
-function Book(title, author, pageCount, readStatus) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
-    this.pageCount = pageCount;
-    this.readStatus = readStatus;
+    this.pages = pages;
+    this.read = read;
 
     this.info = function() {
         let readStatusText = this.readStatus ? 'read' : 'not read yet';
@@ -13,11 +13,21 @@ function Book(title, author, pageCount, readStatus) {
 }
 
 function addBookToLibrary(title, author, pages, read) {
-
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let pages = document.querySelector('#pages').value;
+    let read = document.querySelector('#read').checked;
+    let newBook = new Book(title, author, pages, read);
+    console.log(newBook);
 }
 
 let newBookbtn = document.querySelector('#new-book-btn');
 newBookbtn.addEventListener('click', function() {
     let newBookForm = document.querySelector('#new-book-form');
     newBookForm.style.display = 'block';
+})
+
+document.querySelector('new-book-form').addEventListener('submit', function(){
+    event.preventDefault();
+    alert('HELLO WORLD');
 })
